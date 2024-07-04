db633ff8399c22d27a415adf56d3b53416bce69c
Kristian Perez <SirKriSftw@hotmail.com>
2024-07-04 15:09:51 -0400

    Can now add/remove choices. Pressing save question currently just console logs question, answer and choices.

--- src/app/components/new-question/new-question.component.ts
+++ src/app/components/new-question/new-question.component.ts
@@ -10,20 +10,37 @@ export class NewQuestionComponent {
   @Input() testId: number = 0;
   question: string = "";
   answer: string = "";
-  choices: string[] = ["a", "b"];
+  choices: string[] = [];
   MAX_CHOICES: number = 26;
 
-  choicePlaceholder(i: number)
+  choiceLabel(i: number)
   {
     const asciiA = "a".charCodeAt(0);
     const asciiValue = i + asciiA;
     const optionLetter = String.fromCharCode(asciiValue);
 
-    return `Choice ${optionLetter}.`
+    return `${optionLetter}.`
   }
 
   addChoice()
   {
     this.choices.push("");
+  }
+
+  removeChoice(i: number)
+  {
+    this.choices.splice(i, 1);
+  }
+  
+  trackByFn(index: number)
+  {
+    return index;
+  }
+
+  saveQuestion()
+  {
+    console.log(this.question);
+    console.log(this.choices);
+    console.log(this.answer);
   }
 }

0d74e1ae3d1c13c1952468a28da03f63f73ae700
Kristian Perez <SirKriSftw@hotmail.com>
2024-07-04 12:47:49 -0400

    Added bool to determine if answers should be hidden or not

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -12,6 +12,7 @@ export class TestComponent {
 
   test?: Test;
   id?: number;
+  hideAnswers: boolean = false;
 
   constructor(private testsService: TestsService,
               private route: ActivatedRoute

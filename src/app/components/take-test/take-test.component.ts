80b04e4ec94ac99626d08a9801539da8d55bd3ae
Kristian Perez <SirKriSftw@hotmail.com>
2024-07-04 21:10:28 -0400

    Can now paginate through questions of selected test thats being taken

--- src/app/components/take-test/take-test.component.ts
+++ src/app/components/take-test/take-test.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Test } from '../../models/test.model';
 import { TestsService } from '../../services/tests.service';
 import { ActivatedRoute } from '@angular/router';
+import { Question } from '../../models/question.model';
 
 @Component({
   selector: 'app-take-test',
@@ -11,8 +12,10 @@ import { ActivatedRoute } from '@angular/router';
 export class TakeTestComponent {
 
   options: any = {};
-  test?: Test;
+  test?: Test = undefined;
   id: number = -1;
+  questionIndex = 0;
+  currentQuestion?: Question = undefined;
 
   constructor(private testsService: TestsService,
               private route: ActivatedRoute
@@ -24,8 +27,38 @@ export class TakeTestComponent {
     this.testsService.getTest(this.id).subscribe(
       (r) => {
         this.test = r;
+        if(this.test?.questions) this.currentQuestion = this.test?.questions[this.questionIndex];
       }
     );
     this.options = history.state.info;
+    console.log(this.options);
+  }
+
+  choiceLabel(i: number)
+  {
+    const asciiA = "a".charCodeAt(0);
+    const asciiValue = i + asciiA;
+    const optionLetter = String.fromCharCode(asciiValue);
+
+    return `${optionLetter}.`
+  }
+
+  nextQuestion()
+  {
+    if(this.test?.questions)
+    {
+      if(this.questionIndex + 1 < this.test?.questions.length) this.questionIndex++;
+      this.currentQuestion = this.test?.questions[this.questionIndex];
+    }
+    
+  }
+
+  prevQuestion()
+  {
+    if(this.test?.questions)
+      {
+        if(this.questionIndex - 1 >= 0) this.questionIndex--;
+        this.currentQuestion = this.test?.questions[this.questionIndex];
+      }
   }
 }

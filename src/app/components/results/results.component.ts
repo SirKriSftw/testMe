16009f1630cb13a946409373fabd733420b2c91e
Kristian Perez <SirKriSftw@hotmail.com>
2024-07-06 16:59:24 -0400

    Installed chart.js made a temp chart to display correct/wrong answers in results page

--- src/app/components/results/results.component.ts
+++ src/app/components/results/results.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-results',
@@ -8,10 +9,59 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ResultsComponent {
 
-  constructor(private route: ActivatedRoute) {}
+  @ViewChild("resultsChart") resultsChart!: ElementRef;
+  chart: any;
+
+  results: any = {};
+  questionCount: number = 0;
+  totalWrong: number = 0;
+  hideCorrect: boolean = false;
+  hideWrong: boolean = false;
+
+  constructor(private router: Router) {}
 
   ngOnInit()
   {
-    console.log(history.state.results);
+    this.results = history.state.results;
+    if(this.results === undefined) this.router.navigate([""]);
+    this.questionCount = this.results.wrongQuestions.length + this.results.correctQuestions.length;
+    this.totalWrong = this.questionCount - this.results.totalCorrect;
+  }
+
+  ngAfterViewInit()
+  {
+    const ctx = this.resultsChart.nativeElement.getContext("2d");
+
+    const data = {
+      labels: [
+        'Wrong',
+        'Correct'
+      ],
+      datasets: [{
+        label: "Total",
+        data: [this.totalWrong, this.results.totalCorrect],
+        backgroundColor: [
+          "red",
+          "green"
+        ],
+        hoverOffset: 4
+      }],
+    }
+
+    const options = {
+      responsive: true,
+      plugins: {
+        legend: {
+          display: false
+        }
+      }
+    }
+
+    this.chart = new Chart(ctx, {
+      type: "pie",
+      data: data,
+      options: options
+    })
+
   }
 }

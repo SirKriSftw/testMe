e9a2fc9469ad17b21137356f5a401731bc63c4fa
Kristian Perez <SirKriSftw@hotmail.com>
2024-07-08 11:13:34 -0400

    Made user model and loggedInUser function in auth service.

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { from } from 'rxjs';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,16 @@ import { Injectable } from '@angular/core';
 export class AuthenticationService {
 
   constructor() { }
+
+  loggedInUser()
+  {
+    return from(new Promise<User>((resolve, reject) => {
+      const user = {
+        id: 1,
+        email: "my@email.com",
+        username: "Ad Min"
+      }
+      resolve(user);
+    }));
+  }
 }
